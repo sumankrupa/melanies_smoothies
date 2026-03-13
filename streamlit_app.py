c9d5c68271f730781c56333213d623c01e688773
sumankrupa <53121013+sumankrupa@users.noreply.github.com>
2026-03-13 17:44:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,35 @@
 # Import python packages
 import streamlit as st
 
+# Title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
 
-# Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """
-)
-
+# Name input
 name_on_the_order = st.text_input('Name on Smoothie:')
-st.write('The name on your smoothie will be',name_on_the_order)
+st.write('The name on your smoothie will be', name_on_the_order)
 
-cnx =st.connection('snowflake')
-session = cnx.session()
+# Connect to Snowflake
+cnx = st.connection('snowflake')
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_name'))
+# Get fruit options
+my_dataframe = cnx.query("SELECT FRUIT_NAME FROM smoothies.public.fruit_options")
 
-
+# Ingredient selector
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients',
-    my_dataframe,
-    max_selections = 5
+    'Choose up to 5 ingredients:',
+    my_dataframe['FRUIT_NAME']
 )
+
 if ingredients_list:
-    ingredients_string = ''
-    for i in ingredients_list:
-        ingredients_string += i + ' '
-    
+    ingredients_string = ' '.join(ingredients_list)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                        values ('""" + ingredients_string + """','"""+name_on_the_order+"""')"""
-    time_to_insert = st.button('Submit Order')   
+    my_insert_stmt = f"""
+        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+        VALUES ('{ingredients_string}', '{name_on_the_order}')
+    """
+
+    time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-
+        cnx.query(my_insert_stmt)
+        st.success(f'Your Smoothie is ordered, {name_on_the_order}!', icon="✅")
