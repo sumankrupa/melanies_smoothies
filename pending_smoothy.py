7ed661d0f13e4f86205517c8415588d81061a156
sumankrupa <53121013+sumankrupa@users.noreply.github.com>
2026-03-13 17:14:13 -0500

    Create pending_smoothy.py
    initial page

---
+++ pending_smoothy.py
@@ -0,0 +1,38 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import when_matched
+
+# Write directly to the app
+st.title(f":cup_with_straw: Pending Smoothie Orders!:cup_with_straw:")
+st.write(
+  """Orders that need to be filled.
+  """
+)
+
+
+
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.orders")
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    
+    if submitted:
+        st.success('Someone clicked the button.')
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            
+            og_dataset.merge(edited_dataset
+                             , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                             , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                            )
+            st.success('Order(s) Updated!')
+        except:
+            st.write('Something is wrong')
+else:
+    st.success('There no more pending orders')
